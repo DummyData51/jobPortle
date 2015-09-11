67138e2a86949f1f6171f9b6a6272ba140758092
mateenyaseen <mateen.yaseen1@gmail.com>
2015-09-11 16:25:28 -0700

    Fragment activity change

--- app/src/main/java/com/example/mhr19/jobportle/FragmentAdapter.java
+++ app/src/main/java/com/example/mhr19/jobportle/FragmentAdapter.java
@@ -45,7 +45,7 @@ public class FragmentAdapter extends FragmentPagerAdapter {
         Locale l = Locale.getDefault();
         switch (position) {
             case 0:
-                return "JOBSS";
+                return "JOBSSS";
             case 1:
 
                 return "SAVED JOBS";
